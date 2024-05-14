9ccc3e9c592b19e6cc535687f3cf82a8022cbed5
ZZ3n <ndsrhkd@naver.com>
2024-05-14 16:29:31 +0900

    add buttons

--- src/components/pages/TestRootPage.jsx
+++ src/components/pages/TestRootPage.jsx
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
+import SmallLayout from "components/layouts/SmallLayout";
+import LongButton from "components/buttons/LongButton";
 
 function TestRootPage() {
   const style = {
     fontSize: "32px",
     margin: "10px 10px",
   }
-  return <>
-    <Link to="/login" style={style}>로그인</Link>
-    <Link to="/signup" style={style}>회원가입</Link>
-    <Link to="/posts" style={style}>글 목록</Link>
-    <Link to="/posts/detail" style={style}>글 상세</Link>
-    <Link to="/posts/write" style={style}>글 쓰기</Link>
-    <Link to="/posts/edit" style={style}>글 수정</Link>
-    <Link to="/me/edit" style={style}>회원정보 수정</Link>
-    <Link to="/me/edit-password" style={style}>비밀번호 수정</Link>
-  </>
+  return <SmallLayout>
+    <Link to="/login" style={style}><LongButton>로그인</LongButton></Link>
+    <Link to="/signup" style={style}><LongButton>회원가입</LongButton></Link>
+    <Link to="/posts" style={style}><LongButton>글 목록</LongButton></Link>
+    <Link to="/posts/detail" style={style}><LongButton>글 상세</LongButton></Link>
+    <Link to="/posts/write" style={style}><LongButton>글 쓰기</LongButton></Link>
+    <Link to="/posts/edit" style={style}><LongButton>글 수정</LongButton></Link>
+    <Link to="/me/edit" style={style}><LongButton>회원정보 수정</LongButton></Link>
+    <Link to="/me/edit-password" style={style}><LongButton>비밀번호 수정</LongButton></Link>
+  </SmallLayout>
 }
 
 export default TestRootPage;
